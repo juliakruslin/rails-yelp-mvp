7b0b30a15f7df55e4c28f19dd070898eae54f30c
Julia Kruslin <julia.kruslin@gmail.com>
2020-11-18 16:51:32 +0100

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+
+  resources :reviews, only: [:new, :create]
+  end
 end
+
+
+  # get 'restaurants/top', to: 'restaurants#top', as: :top_restaurants
+  # get 'restaurants/:id/chef', to: 'restaurants#chef', as: :chef_restaurant
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p rest1 = Restaurant.create!(name: "Bli", phone_number: 0650432123, address:"banana street",category: "belgian")
+p rest2 = Restaurant.create!(name:"Bla", phone_number: 0650432123, address:"vanilla street",category: "french")
+p rest3 = Restaurant.create!(name:"Blub", phone_number: 0650432123, address: "coding street",category: "japanese")
+p rest4 = Restaurant.create!(name:"what", phone_number: 0650432123, address:"whatver street",category: "italian")
+p rest5 = Restaurant.create!(name:"kidsss", phone_number: 0650432123, address:"hihi street",category: "chinese")
